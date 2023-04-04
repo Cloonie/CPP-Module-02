5944a42d76f67754281879b2586e809ab386a0a9
root <rsdoot@DESKTOP-T5CUUII.localdomain>
2023-04-05 01:04:12 +0800

    .

---
+++ ex00/Fixed.cpp
@@ -0,0 +1,31 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed()
+{
+	integer = 0;
+}
+
+Fixed::Fixed(const Fixed& other)
+{
+
+}
+
+Fixed& operator=(const Fixed& other)
+{
+
+}
+
+Fixed::~Fixed()
+{
+
+}
+
+int getRawBits( void ) const
+{
+
+}
+
+void setRawBits( int const raw )
+{
+
+}
