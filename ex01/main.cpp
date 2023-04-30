f49cbadceb1e20c856c16aeaf9d57cf46ed8015d
root <rsdoot@DESKTOP-T5CUUII.localdomain>
2023-04-30 16:14:11 +0800

    remove uncessary files

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -2,29 +2,29 @@
 
 int	main(void)
 {
-	// Fixed		a;
-	// Fixed const b( 10 );
-	// Fixed const c( 42.42f );
-	// Fixed const d( b );
+	Fixed		a;
+	Fixed const b( 10 );
+	Fixed const c( 42.42f );
+	Fixed const d( b );
 
-	// a = Fixed( 1234.4321f );
+	a = Fixed( 1234.4321f );
 
-	// std::cout << "a is " << a << std::endl;
-	// std::cout << "b is " << b << std::endl;
-	// std::cout << "c is " << c << std::endl;
-	// std::cout << "d is " << d << std::endl;
+	std::cout << "a is " << a << std::endl;
+	std::cout << "b is " << b << std::endl;
+	std::cout << "c is " << c << std::endl;
+	std::cout << "d is " << d << std::endl;
 
-	// std::cout << "a is " << a.toInt() << " as integer" << std::endl;
-	// std::cout << "b is " << b.toInt() << " as integer" << std::endl;
-	// std::cout << "c is " << c.toInt() << " as integer" << std::endl;
-	// std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+	std::cout << "a is " << a.toInt() << " as integer" << std::endl;
+	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
+	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
+	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
 
-	Fixed abc(10);
+	// Fixed abc(10.10f);
 
-	std::cout << abc << std::endl;
-	std::cout << abc._value << std::endl;
-	std::cout << abc._bits << std::endl;
-	std::cout << abc.toFloat() << std::endl;
-	std::cout << abc.toInt() << std::endl;
+	// std::cout << abc << std::endl;
+	// std::cout << abc._value << std::endl;
+	// std::cout << abc._bits << std::endl;
+	// std::cout << abc.toFloat() << std::endl;
+	// std::cout << abc.toInt() << std::endl;
 	return 0;
 }
